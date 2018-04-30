32fca7755db8d212d44c129b652957ea207d0eaf
Benjamin <rose.benjamin925@gmail.com>
2018-04-30 14:26:52 -0700

    added logic for parsing argv

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-require('colors');
 
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
@@ -8,8 +7,49 @@ const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 const req = require('request');
 
-client.get('statuses/user_timeline', {screen_name: 'benji_rows'}, function (error, tweets, response) {
-  if (!error) {
-    console.log(JSON.stringify(tweets, null, 2).rainbow);
+
+global.ops = {
+
+
+  my_tweets: () => {
+    console.log('twitter');
+
+    client.get('statuses/user_timeline', {screen_name: 'nodejs'},
+      function (error, tweets, response) {
+        if (!error) {
+          console.log(JSON.stringify(tweets, null, 2));
+        }
+      });
+  },
+
+  spotify_this_song: song => {
+    console.log('spotify')
+
+  },
+
+  movie_this: movie => {
+    console.log('omdb')
+
+  },
+
+  do_what_it_says: thing => {
+    console.log('do it')
+
   }
-});
+};
+
+function takeInput() {
+  const service = process.argv[2].replace(/-/g, '_').toLowerCase();
+  const command = process.argv
+    .splice(3, process.argv.length - 2).join().replace(/,/g, ' ');
+  try {
+  global.ops[service](
+    typeof command !== 'undefined' ? command : null)
+  }
+  catch (e) {
+    console.log('invalid command \n' + e)
+  }
+}
+
+
+takeInput();
